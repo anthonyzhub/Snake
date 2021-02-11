3547625fc57ce720bb1540ff929fe157b61739d1
Anthony <zamora.anthony1234@gmail.com>
2021-02-10 23:09:57 -0500

    Added window

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #! /usr/bin/python3
 
+import pygame
+
+# Screen dimensions
+SCREEN_LENGTH = 840
+SCREEN_HEIGHT = 720
+
 if __name__ == "__main__":
-    print("Hello")
+    
+    # Initialize pygame
+    pygame.init()
+
+    # Create a window
+    screen = pygame.display.set_mode((SCREEN_LENGTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Anthony's Snake Game")
+    
+    # Quit pygame
+    pygame.quit()
