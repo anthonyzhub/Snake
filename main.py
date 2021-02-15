b053570d824b5c345489b98aeb3ed2b63e75d52c
Anthony <zamora.anthony1234@gmail.com>
2021-02-14 21:06:54 -0500

    Added snake's head

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 #! /usr/bin/python3
 
 import pygame
+from snake import Snake
+
+# Colors
+BLACK = (0, 0, 0)
+LIME = (0, 255, 0)
+WHITE = (255, 255, 255)
 
 # Screen dimensions
 SCREEN_LENGTH = 840
 SCREEN_HEIGHT = 720
+
+SCREEN_LENGTH_MID = SCREEN_LENGTH // 2
+SCREEN_HEIGHT_MID = SCREEN_HEIGHT // 2
+
+def updateScreen(screen, clock, allSpritesList):
+
+    # OBJECTIVE: Update screen 
+
+    # Turn screen black
+    screen.fill(BLACK)
+
+    # Draw sprites
+    allSpritesList.draw(screen)
+
+    # Update screen
+    pygame.display.flip()
+
+    # FPS
+    clock.tick(60)
 
 if __name__ == "__main__":
     
@@ -14,6 +39,36 @@ if __name__ == "__main__":
     # Create a window
     screen = pygame.display.set_mode((SCREEN_LENGTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Anthony's Snake Game")
-    
+
+    # Create a sprite
+    snake = Snake(LIME, 15, 15)
+    snake.rect.x = SCREEN_LENGTH_MID
+    snake.rect.y = SCREEN_HEIGHT_MID
+
+    # Add all sprites to list
+    allSpritesList = pygame.sprite.Group()
+    allSpritesList.add(snake)
+
+    # Create boolean variable to stop/start game
+    continueGame = True
+
+    # Initialize pygame's clock
+    clock = pygame.time.Clock()
+
+    while continueGame:
+
+        # Look for keyboard inputs
+        for event in pygame.event.get():
+
+            # True, when "close window" button is closed
+            if event.type == pygame.QUIT: 
+                continueGame = False
+
+        # Update all sprites
+        allSpritesList.update()
+
+        # Update screen
+        updateScreen(screen, clock, allSpritesList)
+
     # Quit pygame
     pygame.quit()
---
+++ snake.py
@@ -0,0 +1,34 @@
+import pygame
+
+LIME = (0, 255, 0)
+WHITE = (255, 255, 255)
+
+class Snake(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+
+        # Initialize parent class
+        super().__init__()
+
+        # Create snake's body
+        self.image = pygame.Surface([width, height])
+        self.image.fill(LIME)
+        self.image.set_colorkey(WHITE)
+
+        # Draw snake on the screen
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+
+        # Get snake's dimensions
+        self.rect = self.image.get_rect()
+
+    def moveLeft(self, pixels):
+        self.rext.x -= pixels
+
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+
+    def moveDown(self, pixels):
+        self.rect.y += pixels
+
+    def moveUp(self, pixels):
+        self.rect.y -= pixels
